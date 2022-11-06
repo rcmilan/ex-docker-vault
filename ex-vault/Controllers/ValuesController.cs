5f0ca84ab8fb1df9ec5664f22c2c7a52a98c2f8f
Ricardo Milan <ricardo@fluencypass.com>
2022-11-06 08:15:26 -0300

    web api

---
+++ ex-vault/Controllers/ValuesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ex_vault.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
